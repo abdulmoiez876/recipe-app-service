f3ff98d4e9eb5fb65e1e6f4fb676f5ccdf23b163
abdulmoiez876 <abdul.moiez876@gmail.com>
2022-12-13 16:05:18 +0500

    recipe service implemented

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-
+import { RecipeService } from '../../shared/services/recipe.service';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,19 +8,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-  ];
+  private recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.recipes;
+  }
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
   }
 
   onRecipeSelected(recipe: Recipe) {
-   this.recipeWasSelected.emit(recipe);
+  //  this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/shared/services/recipe.service.ts
+++ src/app/shared/services/recipe.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../../recipes/recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
+
+  
 
   constructor() { }
 }
